e82476d659944e20559b5ac4855a3b5c89a86e43
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:27:25 +0800

    Square Function

--- Calculator5.js
+++ Calculator5.js
@@ -6,7 +6,13 @@ function subtract(a,b){
     return a+b
 }
 
+
+function square(a){
+    return a*a
+}
+
 a = 5
 b = 3
 console.log("ADDITION:", add(a,b))
 console.log("SUBTRACTION:", subtract(a,b))
+console.log("SQUARE", square(a))
